5304272fda4da2b4e4640b80440e7500146cfc18
Ahmeeed22 <ahmeeedzezo33@gmail.com>
2023-08-16 18:52:25 +0400

    add new role

--- common/middleare/isAuth.js
+++ common/middleare/isAuth.js
@@ -9,16 +9,17 @@ module.exports = (authRole="ALL")=>{
                 const {id,role , company_id,name,email}=jwt.verify(token,'alsham2332') ;
                 if (id) {
                     req.loginData={id , role , company_id,name,email}
+                 
                     if (authRole == "ALL") {
                         next()     
                     } else {
-                        role===2 ? next() : res.status(StatusCodes.UNAUTHORIZED).json({message:"Not Authenticated"})
+                        role===2 || role===3 ? next() : res.status(StatusCodes.UNAUTHORIZED).json({message:"Not Authenticated"})
                     }
                 } else {
                     res.status(StatusCodes.UNAUTHORIZED).json({message:"Not Authenticated"})
                 }
             } catch (error) {
-                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error}) 
             }
         } else {
             res.status(StatusCodes.UNAUTHORIZED).json({message:"Not Authenticated"}) 
